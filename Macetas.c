bc6241ebead968865bfa22b90f6e476c4b71fc33
ElizabethRosse <eli_mure@ciencias.unam.mx>
2015-03-23 21:06:16 -0600

    pequeño bug resuelto

--- Macetas.c
+++ Macetas.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
+/*
+  UNAM, Facultad de Ciencias.
+  Modelado y programación
+  Muñiz Rivas Elizabeth
+ */
+
 int main(){
   int i = 0;
-  int tam;
   char *cadena = (char*)malloc(sizeof(char)*1000);
-
+  char *salida = (char*)malloc(sizeof(char)*1000);
+  
   printf("Escribe la secuencia: ");
   scanf("%s", cadena);
- 
-  while (cadena[i] != NULL){
-    tam ++;
-    i ++;
-  }
-  char salida[tam];
-  i = 0;
+
+  //la lampara quedará lo más a la derecha posible
   while (cadena[i] == 'X' || cadena[i] == '_'){
     if (cadena[i] == '_')
       salida[i] = '.';      
@@ -24,7 +25,7 @@ int main(){
 	salida[i+2] = '.';
       i +=2;
     }
-    if(cadena[i] != 'X' && cadena[i] != '_')
+    if(cadena[i] != 'X' && cadena[i] != '_') // marcamos el final de la cadena
       salida[i] = '\0';
     else
       i += 1;
